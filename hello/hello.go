58b0a827a9ea6b0534cc3532b2ea0962bae729da
ELIJAHKUNGU <elijahkungu100@gmail.com>
2022-07-20 21:09:52 +0300

    hello world

---
+++ hello/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var studentname string = "ELIJAH"
+	// var studentAge int = 67
+
+	// println(studentname)
+	// println(studentAge)
+
+	var (
+		a int
+		b int    = 1
+		c string = "hello"
+	)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+}
